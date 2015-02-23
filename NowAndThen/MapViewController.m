7047fe3a9b7d3facc08fb96294149158d10dbc58
Josh Kahl <j7kahl@gmail.com>
2015-02-22 22:29:27 -0800

    "added start location zoom and button outlets"

--- NowAndThen/MapViewController.m
+++ NowAndThen/MapViewController.m
@@ -15,45 +15,82 @@
 @property (strong, nonatomic) MKMapView *mapView;
 @property (strong, nonatomic) CLLocationManager *locationManager;
 
+@property (weak, nonatomic) IBOutlet UIButton *centerOnUser;
+-(IBAction)centerOnUser:(id)sender;
+
+
 @end
 
 @implementation MapViewController
 
+//when leaving the mapview - allow the user to chose to update the maps location if they move
+
+
 #pragma mark - UIViewController Lifecycle
-- (void)loadView {
-    
-    self.mapView.frame = [[UIScreen mainScreen] bounds];
-    
-    self.view = self.mapView;
+- (void)loadView
+{
+  self.mapView.delegate = self;
+  self.mapView.frame = [[UIScreen mainScreen] bounds];
+  self.view          = self.mapView;
+  
+  //[self.mapView addSubview:self.centerOnUser];
+  self.centerOnUser.backgroundColor = [UIColor blueColor];
+  //[self.view addSubview:self.centerOnUser];
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    self.title = @"MapView";
-    
-    [self setupCoreLocationAuthorization];
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+  self.title = @"MapView";
+  
+  [self setupCoreLocationAuthorization];
+  self.mapView.alpha = 0;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+  CLLocationCoordinate2D startLocation;
+ // startLocation.latitude = self.mapView.userLocation.coordinate.latitude;
+ // startLocation.longitude = self.mapView.userLocation.coordinate.longitude;
+  startLocation.latitude = 47.6204;    //TODO: these are just temp variables due to simulator issues
+  startLocation.longitude = -122.3491;
+  MKCoordinateRegion startRegion = MKCoordinateRegionMakeWithDistance(startLocation, 750, 750);
+  
+  [UIView animateWithDuration:2.5 animations:^{
+    self.mapView.alpha = 1.0;
+   [self.mapView setRegion:startRegion];
+  }];
+//  [self.mapView setRegion:startRegion];
+
 }
 
 #pragma mark - CoreLocation
-- (void)setupCoreLocationAuthorization {
-    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
-        [self.locationManager requestWhenInUseAuthorization];
-    } else {
-        self.mapView.showsUserLocation = true;
-        [self.locationManager startUpdatingLocation];
-    }
-    
+- (void)setupCoreLocationAuthorization
+{
+  NSLog(@"coreLocation");
+  if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined)
+  {
+    [self.locationManager requestWhenInUseAuthorization];
+  } else {
+    self.mapView.showsUserLocation = true;
+    [self.locationManager startUpdatingLocation];
+    [self.mapView setZoomEnabled:true];
+    [self.mapView setScrollEnabled:true];
+  }
 }
 
 #pragma mark - MKMapViewDelegate
--(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
-    MKCoordinateRegion mapRegion;
-    mapRegion.center = self.mapView.userLocation.coordinate;
-    mapRegion.span.latitudeDelta = 0.6;
-    mapRegion.span.longitudeDelta = 0.6;
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+  NSLog(@"mapviewDelegate");
+  MKCoordinateRegion mapRegion;
+  mapRegion.center = self.mapView.userLocation.coordinate;
+  mapRegion.span.latitudeDelta = 0.6;
+  mapRegion.span.longitudeDelta = 0.6;
     
-    [self.mapView setRegion:mapRegion animated:true];
+  [self.mapView setRegion:mapRegion
+                 animated:true];
 }
 
 
@@ -70,6 +107,20 @@
         _locationManager = [[CLLocationManager alloc] init];
     }
     return _locationManager;
+}
+
+-(UIButton *)centerOnUser
+{
+  if (!_centerOnUser)
+  {
+    _centerOnUser = [[UIButton alloc] initWithFrame:CGRectMake(50, 50, 50, 50)];
+  }
+  return _centerOnUser;
+}
+
+-(IBAction)centerOnUser:(id)sender
+{
+  
 }
 
 @end
