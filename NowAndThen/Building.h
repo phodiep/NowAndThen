dd260d631801effa1a33027880c05e754b7b2969
DAKoch <dkoch7591@gmail.com>
2015-02-23 09:25:48 -0800

    Adding 'Building' class

---
+++ NowAndThen/Building.h
@@ -0,0 +1,18 @@
+//
+//  Building.h
+//  NowAndThen
+//
+//  Created by Gru on 02/22/15.
+//  Copyright (c) 2015 Pho Diep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//@interface Building : NSObject
+//
+//-(void) initialize: (NSDictionary*) buildingInformation;
+//-(void) print;
+//-(void) setLatitude: (NSString*) latitude;
+//-(NSString*) getLatitude;
+//
+//@end
