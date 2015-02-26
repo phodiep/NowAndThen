b75cfaebe1899564dd37f4f4c33f5ce701d13849
Josh Kahl <j7kahl@gmail.com>
2015-02-25 16:36:30 -0800

    added a fetch request to pull building info from the server. some images will load in the annotations, but not all annotations have images to load

---
+++ NowAndThen/Building+Annotation.h
@@ -0,0 +1,13 @@
+//
+//  Building+Annotation.h
+//  NowAndThen
+//
+//  Created by Josh Kahl on 2/25/15.
+//  Copyright (c) 2015 Pho Diep. All rights reserved.
+//
+
+#import "Building.h"
+
+@interface Building (Annotation)
+
+@end
---
+++ NowAndThen/Building+Annotation.m
@@ -0,0 +1,13 @@
+//
+//  Building+Annotation.m
+//  NowAndThen
+//
+//  Created by Josh Kahl on 2/25/15.
+//  Copyright (c) 2015 Pho Diep. All rights reserved.
+//
+
+#import "Building+Annotation.h"
+
+@implementation Building (Annotation)
+
+@end
